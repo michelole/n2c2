ea75e28959010c47d98ce2e96c2674a0d7b39ad9
Michel Oleynik <michelole@gmail.com>
2019-04-29 15:50:42 +0200

    Enable `BILSTMEmbeddingsClassifier` in the factory

--- src/main/java/at/medunigraz/imi/bst/n2c2/classifier/factory/NNClassifierFactory.java
+++ src/main/java/at/medunigraz/imi/bst/n2c2/classifier/factory/NNClassifierFactory.java
@@ -2,11 +2,11 @@ package at.medunigraz.imi.bst.n2c2.classifier.factory;
 
 import at.medunigraz.imi.bst.n2c2.classifier.Classifier;
 import at.medunigraz.imi.bst.n2c2.model.Criterion;
-import at.medunigraz.imi.bst.n2c2.nn.LSTMClassifier;
+import at.medunigraz.imi.bst.n2c2.nn.BILSTMEmbeddingsClassifier;
 
 public class NNClassifierFactory implements ClassifierFactory {
 
-    private static final Classifier classifier = new LSTMClassifier();
+    private static final Classifier classifier = new BILSTMEmbeddingsClassifier();
 
 	@Override
 	public Classifier getClassifier(Criterion criterion) {
